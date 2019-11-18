995a0eaeef0ab1931e6ba4dad72acfdbeaccb6ff
Jonathan Scholbach <j.scholbach@posteo.de>
2019-11-18 10:16:27 +0100

    #5 add matomo script to layouts/master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -22,7 +22,23 @@
   
 <!-- custom styles -->
   <link rel="stylesheet" type="text/css" href="/css/master.css">
- 
+
+<!-- Matomo -->
+<script type="text/javascript">
+  var _paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//nun-singet-und-seid-froh.info/matomo/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
+
   @yield('head')
 </head>
 
