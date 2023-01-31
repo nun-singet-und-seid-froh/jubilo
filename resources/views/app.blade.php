980fc6c96c703ac8130ad2c1482607e4f2131004
Johannes Ballmann <574988+fibsifan@users.noreply.github.com>
2023-01-31 08:18:13 +0100

    Fix routing

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,14 +7,17 @@
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+	    {{-- TODO: remove google dependency --}}
+	    <link rel="preconnect" href="https://fonts.googleapis.com">
+	    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-serif antialiased">
         @inertia
     </body>
 </html>
