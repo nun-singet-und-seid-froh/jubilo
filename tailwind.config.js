7fa5d5610d8f2b4bf1dbecbc9d29d261ea476f31
Johannes Ballmann <574988+fibsifan@users.noreply.github.com>
2023-02-05 14:39:32 +0100

    add EBGaramond 016

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
-    ],
+	content: [
+		'./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+		'./vendor/laravel/jetstream/**/*.blade.php',
+		'./storage/framework/views/*.php',
+		'./resources/views/**/*.blade.php',
+		'./resources/js/**/*.vue',
+	],
 
     theme: {
         extend: {
@@ -16,20 +16,20 @@ module.exports = {
 				serif: ['"EB Garamond 12"', ...defaultTheme.fontFamily.serif]
 			},
 	        colors: {
-				background: '#0a0a2a',
-				prussian: {
-					50: '#262629',
-					100: '#242429',
-					200: '#1c1c29',
-					300: '#181829',
-					400: '#141429',
-					500: '#111129',
-					600: '#0d0d29',
-					700: '#0a0a2a',
-					800: '#060629',
-					900: '#020229',
-				},
-		        offwhite: '#f2eae8'
+		        'background': '#0a0a2a',
+		        'prussian': {
+			        '50': '#3d3d47',
+			        '100': '#383847',
+			        '200': '#2c2c47',
+			        '300': '#272747',
+			        '400': '#212147',
+			        '500': '#1b1b47',
+			        '600': '#151547',
+			        '700': '#101048',
+			        '800': '#090947',
+			        '900': '#040447',
+		        },
+		        'offwhite': '#f2eae8'
 	        }
         },
     },
